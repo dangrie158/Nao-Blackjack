f30615ed98c3320f9a641b4eb69a2123f5cc5a5b
Daniel Grießhaber <dangrie158@gmail.com>
2015-06-25 20:03:46 +0200

    added textual representation of detected value to game table

--- Evaluation/lib/VirtualTable.py
+++ Evaluation/lib/VirtualTable.py
@@ -4,6 +4,9 @@ from math import ceil
 import os
 import sys
 import HelperFunctions as hf
+from RecognitionEngine import Value
+
+FONT = cv2.FONT_HERSHEY_SIMPLEX
 
 class VirtualTable:
 	def __init__(self, title):
@@ -26,13 +29,10 @@ class VirtualTable:
 			cardThumbnail = card.getThumbnail((cardWidth, cardHeight))
 			#cardThumbnail = card.getValueImage()
 
-			## writing one detected file to disk:
-			#wert = self.cardname
-			#farbe = "karo"
-			#filename = wert + "_" + farbe
-			#filename = filename + ".png"
-			#filename = os.path.join(writePath, filename)
-			#cv2.imwrite(filename, card.image)
+			#convert thumbnail back to RGB
+			cardThumbnail = cv2.cvtColor(cardThumbnail, cv2.COLOR_GRAY2RGB)
+
+			cardText = card.value.name
 			
 			#check where (x) the new card should go
 			nextX = start[0] + dx * (cardWidth + margin)
@@ -46,6 +46,8 @@ class VirtualTable:
 				nextY = start[1] + dy * (cardHeight+margin)
 
 			frame[nextY:nextY+cardThumbnail.shape[0], nextX:nextX+cardThumbnail.shape[1]] = cardThumbnail
+			color = (0,255,0) if card.value != Value.Undefined else (255,0,0)
+			cv2.putText(frame, cardText, (int(nextX),int(nextY + cardThumbnail.shape[0])), FONT, 0.6, color, 1, cv2.CV_AA)
 			dx = dx + 1
 
 	def render(self, cardWidth = 70, cardHeight = 100, margin = 20, playerMargin = 50, cardPerRow = 5.0):
@@ -58,6 +60,7 @@ class VirtualTable:
 
 
 		frame = np.zeros((frameHeight, frameWidth), dtype = np.uint8)
+		frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
 		
 		self.renderCards(frame, self.player1.cards, (0, 0), margin, cardWidth, cardHeight)
 		dividerY = int(player1heightOnTable + playerMargin / 2)
