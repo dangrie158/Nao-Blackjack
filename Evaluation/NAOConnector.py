13b8943f591f13302bef4a7006f52f5ab938b98b
Stefan Seibert <mail@stefanseibert.com>
2015-06-29 17:52:21 +0200

    Added a Listener so that touch events can be received by callback.

--- Evaluation/NAOConnector.py
+++ Evaluation/NAOConnector.py
@@ -1,25 +1,34 @@
 # NAO Modules Import
 from naoqi import ALProxy
+from naoqi import ALModule
+from naoqi import ALBroker
 import almath
 
 # Other Stuff
 import numpy as np
 import cv2
+from datetime import datetime as dt
+from sys import exit
 
+class NAO():
 
-class NAO:
 	def __init__(self, IPAdress, PortNumber):
 		self.IP = IPAdress
 		self.PORT = PortNumber
 
-
-	def init(self):
+	def setup(self):
 		try:
 			self.motion = ALProxy("ALMotion", self.IP, self.PORT)
 		except Exception, e:
 			print "Could not create Connection to NAO Motion"
 			print "NAO Error: ", e
 	
+		try:
+			self.memory = ALProxy("ALMemory", self.IP, self.PORT)
+		except Exception, e:
+			print "Could not create Connection to NAO Motion"
+			print "NAO Error: ", e
+
 		try:
 			self.video = ALProxy('ALVideoDevice', self.IP, self.PORT)
 		except Exception, e:
@@ -32,31 +41,84 @@ class NAO:
 			print "Could not create Connection to NAO Posture"
 			print "NAO Error: ", e
 
-		if hasattr(self,'motion') and hasattr(self,'posture') and hasattr(self,'video'):
+		try:
+			self.speech = ALProxy('ALTextToSpeech', self.IP, self.PORT)
+		except Exception, e:
+			print "Could not create Connection to NAO Speech"
+			print "NAO Error: ", e
+
+		try:
+			self.behavior = ALProxy('ALBehaviorManager', self.IP, self.PORT)
+		except Exception, e:
+			print "Could not create Connection to NAO Behavior"
+			print "NAO Error: ", e
+
+		if hasattr(self, 'behavior') and hasattr(self, 'memory') and hasattr(self,'motion') and hasattr(self,'posture') and hasattr(self,'video') and hasattr(self, 'speech'):
+			Connector = self
 			return True
 		else:
+			print "NAO Error: Could not initialize all modules correctly."
+			exit(1)
 			return False
+
+	def headTouched(self, value):
+		
+		for v in value:
+			if v[1] and "Head" in v[0]:
+				print "Touched my Head!"
+
+	def hasBehavior(self, name):
+		if hasattr(self, 'behavior'):
+			return self.behavior.isBehaviorInstalled(name)
+
+	def playBehavior(self, name, post=False):
+		if hasattr(self, 'behavior'):
+			if post == True:
+				taskID = self.behavior.post.runBehavior(name)
+				return taskID
+			else:
+				self.behavior.runBehavior(name)
+				return None
+
+	def stopBehavior(self, name):
+		if hasattr(self, 'behavior'):
+			self.behavior.stopBehavior(name)
+
+	def playWinAnimation(self):
+		if hasattr(self, 'behavior') and hasattr(self, 'speech'):
+			taskID = self.playBehavior("animations/Stand/Emotions/Positive/Excited_2", True)
+			self.sayMessage("Yeah, i think i won that game!")
+			self.behavior.wait(taskID, 0)
+		
+	def playLooseAnimation(self):
+		if hasattr(self, 'behavior') and hasattr(self, 'speech'):
+			taskID = self.playBehavior("animations/Stand/Emotions/Negative/Disappointed_1", True)
+			self.sayMessage("Damn I lost. A sad moment for every robot.")
+			self.behavior.wait(taskID, 0)
+		
+	def shutdown(self):
+		self.unsubscribeFromCamera()
 
 	def enableStandardPosture(self):
 		if hasattr(self,'posture'):
-			self.posture.goToPosture("Crouch", 0.5)
-			print "NAO going into Crouch Posture"
-		if hasattr(self,'motion'):
-			self.motion.setIdlePostureEnabled("Head", False)
+			self.posture.goToPosture("Stand", 0.5)
 
-	def setJointPosition(self, bodyPart, angleInput):
+	def setJointPosition(self, bodyPart, angleInput, speed = 0.5):
 		if hasattr(self,'motion'):
-			self.motion.setStiffnesses("Body", 1.0)
 			angle = angleInput * almath.TO_RAD
-			fractionSpeed = 0.4
-			self.motion.setAngles(bodyPart, angle, fractionSpeed)
-			self.motion.setStiffnesses("Body", 0.0)
+			self.motion.setAngles(bodyPart, angle, speed)
 
 	def printMotionState(self):
 		if hasattr(self,'motion'):
 			print self.motion.getSummary()
 
-	def subscribeToCamera(self, cameraNumber = 1, resolutionPix = 3, colorspaceVal = 13, fps = 2):
+	def sayMessage(self, message):
+		if hasattr(self, 'speech'):
+				self.speech.say(message)
+
+	# Camera Number: 0 is upper camera, 1 is lower camera
+	# Resolution: 3 is 1280x960, 2 is 640x480 and so on
+	def subscribeToCamera(self, cameraNumber = 1, resolutionPix = 2, colorspaceVal = 8, fps = 30):
 		self.camera = cameraNumber
 		self.resolution = resolutionPix
 		self.colorspace = colorspaceVal
@@ -71,25 +133,30 @@ class NAO:
 	def getCameraImage(self):
 		if hasattr(self,'video') and hasattr(self,'device'):
 			width, height = self.getWidthHeight()
-			image = np.zeros((height, width, 3), np.uint8)
+
+			startImg = dt.now()
 
 			try:
 				result = self.video.getImageRemote(self.device);
 			except BaseException, e:
 				print("NAO Error: ", e)
+				exit(1)
+
+			retrImg = dt.now()
+			delta1 = retrImg - startImg
+			print "Milliseconds for retrieving Image: " + str(int(delta1.total_seconds()*1000))
 
 			if result == None:
 				print("NAO Error: Cannot capture Image from NAO, maybe restart.")
-				sys.exit(1)
 			else:
 				values = map(ord, list(result[6]))
-				i = 0
-				for y in range(0, height):
-					for x in range(0, width):
-						image.itemset((y, x, 0), values[i + 0])
-						image.itemset((y, x, 1), values[i + 1])
-						image.itemset((y, x, 2), values[i + 2])
-						i += 3
+
+				image = np.reshape(values, (height, width))
+				image = image.astype(np.uint8)
+
+				chImg = dt.now()
+				delta2 = chImg - retrImg
+				print "Milliseconds for reformatting Image: " + str(int(delta2.total_seconds()*1000))		
 				return image
 
 	def getWidthHeight(self):
